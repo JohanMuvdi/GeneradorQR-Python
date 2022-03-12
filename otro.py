5e8992cb0a277107a747740c5b318b11def6b10e
Johan Muvdi <johanmuvd@gmail.com>
2022-03-12 15:51:35 -0400

    otro.py

---
+++ otro.py
@@ -0,0 +1,16 @@
+import qrcode
+from PIL import Image
+
+# Valores de entrada
+nombre_del_participante = input("Nombre y Apellido: ")
+documento_identificacion = input('Documento de identidad: ')
+datos_del_participante = nombre_del_participante + ' ' + documento_identificacion
+codigo_qr = qrcode.make(datos_del_participante)
+
+# Valores del codigo QR
+nombre_imagen = nombre_del_participante + '.png'
+archivo_imagen = open(nombre_imagen, 'wb')
+codigo_qr.save(archivo_imagen)
+archivo_imagen.close()
+
+print(f'El codigo QR de {nombre_del_participante} esta Listo!')
